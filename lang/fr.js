86b4708a7355afcd8d96970e41ecfd8444c0c2ca
PseudoIllyes <illyes.nasser@gmail.com>
2020-06-09 14:10:42 +0200

    ➕/🔨 Add comment and traduction

--- lang/fr.js
+++ lang/fr.js
@@ -1,10 +1,19 @@
 module.exports = {
-    permUser: 'Vous n\'avez pas la permission',
-    setPrefix: 'Prefix changé en : ',
-    githubC: 'Voici mon github : https://github.com/PseudoIllyes/Rem',
-    setPreixArgs: 'Veillez me donner le nouveau prefix !',
-    setLangArgs: 'Veuillez donner la nouvelle langue du bot !',
-    setLang: '[Succcès ✅] Langage changer par : ',
-    setLangErrorA: 'Ce language n\'est pas disponible !'
+    /* Permission */
+    permUser: '`[Erreur ❌] Vous n\'avez pas la permission `',
 
+    /* Prefix */
+    setPrefix: '`[Succcès ✅] Nouveau préfix :` ',
+    setPreixArgs: '`[Erreur ❌] Veillez me donner le nouveau prefix !`',
+    /*Github Command*/
+    githubC: 'Voici mon github : https://github.com/PseudoIllyes/Rem',
+    
+    /*Set-lang Command*/
+    setLangArgs: '`[Erreur ❌] Veuillez spécifier le langage voulu`',
+    setLang: '`[Succcès ✅] La langue du bot à été changé pour :` ',
+    setLangErrorA: '`[Erreur ❌] Ce language n\'est pas disponible !`',
+
+    /* Invite Command */
+
+    inviteC : 'Pour m\'inviter sur un serveur : <https://top.gg/>'
 }
