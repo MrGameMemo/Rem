6c2af75480099348240319d15f1353f6faaae1ee
PseudoIllyes <illyes.nasser@gmail.com>
2020-06-09 14:10:55 +0200

    ➕ Create invite command

---
+++ Commands/invite.js
@@ -0,0 +1,11 @@
+const Discord = require ('discord.js');
+
+module.exports.run = (client, message, args) => {
+
+    message.channel.send(client.lang.inviteC)
+}
+
+module.exports.help = {
+    name: "invite",
+    cooldown: 6
+}
